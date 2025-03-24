6335d7c48e5100f4518c09a24ddbf870e9541afc
Lucas Gomes Santana <ls7128387@gmail.com>
2025-03-24 09:36:56 -0300

    Test branch (#15)
    * Adjusting Project Documentation
    
    * Removing Dockerfiles Temporarily
    
    * Changging API and jar File name

---
+++ SystemAPI/src/main/java/api/Back_End/model/Teacher.java
@@ -0,0 +1,56 @@
+package api.Back_End.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Teachers")
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "teacher_id")
+    private int teacher_id;
+
+    @Column(name = "teacher_name")
+    private String teacherName;
+
+    @Column(name = "teacher_password")
+    private String teacherPassword;
+
+    @ManyToOne
+    @JoinColumn(name = "school_id")
+    @JsonBackReference
+    private School school;
+
+
+    public int getTeacher_id() { return teacher_id; }
+
+    public void setTeacher_id(int teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public String getTeacherPassword() {
+        return teacherPassword;
+    }
+
+    public void setTeacherPassword(String teacherPassword) {
+        this.teacherPassword = teacherPassword;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+}
